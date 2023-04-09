50d87be7a9c974bbf7d65b270748d8744ed11509
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-04-09 16:47:14 +0200

    update test

--- tests/test_ft_char_functions.c
+++ tests/test_ft_char_functions.c
@@ -1,23 +1,22 @@
 #include <assert.h>
-#include <ctype.h>
 #include <stdio.h>
+#include <ctype.h>
+
 #include "libft.h"
 
-void test_char_function(int (*func)(int), int (*ft_func)(int), const char *func_name)
-{
+void test_char_function(int (*func)(int), int (*ft_func)(int), const char *func_name) {
     for (int i = -128; i <= 127; i++) {
         int expected = func(i);
         int actual = ft_func(i);
 
         if (actual != expected) {
-            printf("Test failed: function %s, input %d, expected %d, got %d\n", func_name, i, expected, actual);
+            fprintf(stderr, "Test failed in %s: input %d, expected %d, got %d\n", func_name, i, expected, actual);
             assert(actual == expected);
         }
     }
 }
 
-int main(void)
-{
+int main(void) {
     test_char_function(isalnum, ft_isalnum, "ft_isalnum");
     test_char_function(isalpha, ft_isalpha, "ft_isalpha");
     test_char_function(isascii, ft_isascii, "ft_isascii");
