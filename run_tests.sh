d4bc481b6baa61c5eabd6eccea5a990abcd87d20
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-04-09 16:33:56 +0200

    update test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+exit_status=0
+
 for test_executable in tests/*.out; do
     echo "Running $test_executable"
     ./$test_executable
+    result=$?
+    if [ $result -ne 0 ]; then
+        exit_status=1
+    fi
     echo "-----------------------------"
 done
+
+exit $exit_status
