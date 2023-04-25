709a5b1cf784781c6079955c57c22b0f71144ef2
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-04-25 16:15:11 +0200

    feat: ft_itoa, put functions

---
+++ ft_itoa.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/25 15:35:52 by ebouvier          #+#    #+#             */
+/*   Updated: 2023/04/25 16:12:51 by ebouvier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+int ft_nbrlen(int n)
+{
+    int sz;
+
+    sz = 0;
+    if (n <= 0)
+        sz++;
+    while (n)
+    {
+        n /= 10;
+        sz++;
+    }
+    return (sz);
+}
+
+char    *ft_itoa(int n)
+{
+    char    *new;
+    int     nbr_len;
+    int     sign;
+
+    sign = 0;
+    if (n == -2147483648)
+        return (ft_strdup("-2147483648"));
+    nbr_len = ft_nbrlen(n);
+    if (n < 0)
+    {
+        sign = 1;
+        n = -n;
+    }
+    new = malloc(sizeof(char *) * (nbr_len + 1));
+    if (!new)
+        return (NULL);
+    new[nbr_len] = 0;
+    while (nbr_len)
+    {
+        new[--nbr_len] = n % 10 + '0';
+        n /= 10;
+    }
+    if (sign)
+        new[nbr_len] = '-';
+    return (new);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 13:30:19 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/04/25 14:33:55 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/04/25 15:55:38 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 #define LIBFT_H
 
 #include <stdlib.h>
+#include <unistd.h>
+
+void    ft_putchar_fd(char c, int fd);
+void    ft_putstr_fd(char *s, int fd);
+void    ft_putendl_fd(char *s, int fd);
+void    ft_putnbr_fd(int n, int fd);
+
 
 size_t  ft_strlen(const char *s);
 
@@ -26,13 +33,14 @@ int     ft_isdigit(int c);
 int     ft_isascii(int c);
 int     ft_isalpha(int c);
 
+void    *ft_calloc(size_t nmemb, size_t size);
+
 char    *ft_strdup(const char *s);
+char    *ft_itoa(int n);
 
 void    *ft_memset(void *s, int c, size_t n);
 void    *ft_memmove(void *dest, const void *src, size_t n);
 void    *ft_memcpy(void *dest, const void *src, size_t n);
 void    ft_bzero(void *s, size_t n);
-
-void    *ft_calloc(size_t nmemb, size_t size);
 
 #endif
