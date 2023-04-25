25ab8aaa93bf0d3c4f4734c29df639843f9b05b9
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-04-25 13:37:22 +0200

    feat: memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,22 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 13:30:52 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/04/09 13:31:34 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/04/25 13:33:27 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void    *ft_memset(void *s, int c, size_t n)
+{
+    char    *area;
+    
+    area = s;
+    while (n)
+    {
+        *area = c;
+        area++;
+        n--;
+    }
+    return (s);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 13:30:19 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/04/25 12:58:03 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/04/25 13:36:19 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,17 @@
 
 size_t  ft_strlen(const char *s);
 
-int ft_isalnum(int c);
-int ft_atoi(const char *s);
-int ft_toupper(int c);
-int ft_tolower(int c);
-int ft_isprint(int c);
-int ft_isdigit(int c);
-int ft_isascii(int c);
-int ft_isalpha(int c);
+int     ft_isalnum(int c);
+int     ft_atoi(const char *s);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
+int     ft_isprint(int c);
+int     ft_isdigit(int c);
+int     ft_isascii(int c);
+int     ft_isalpha(int c);
 
-char *ft_strdup(const char *s);
+char    *ft_strdup(const char *s);
+
+void    *ft_memset(void *s, int c, size_t n);
 
 #endif
