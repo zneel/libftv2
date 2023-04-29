cb89425c7cce0a4c8198a8c109fb1ccc8a2b58d4
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-04-29 14:04:52 +0200

    fix: remove o2 and improve build

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 19:50:20 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/04/28 19:50:35 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/04/29 12:28:02 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,28 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*current;
+	t_list	*head;
+	t_list	*tail;
+	t_list	*tmp;
+
+	current = lst;
+	if (!current)
+		return (NULL);
+	head = ft_lstnew((*f)(current->content));
+	if (!head)
+		return (NULL);
+	head->next = NULL;
+	tail = NULL;
+	tmp = head;
+	while (current && tmp)
+	{
+		tail = current;
+		tmp = tmp->next;
+		tmp->next = ft_lstnew((*f)(current->content));
+		current = current->next;
+		(*del)(tail->content);
+		free(tail);
+	}
+	return (head);
 }
