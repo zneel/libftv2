90f78188d025f1f485833ef88d0729c64c64e38c
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-04-29 13:10:11 +0200

    fix: ft_calloc division by zero

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 13:30:32 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/04/28 18:45:08 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/04/29 12:45:49 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t	total_size;
 
 	total_size = nmemb * size;
-	if (total_size / size != nmemb)
+	if (size != 0 && total_size / size != nmemb)
 		return (NULL);
 	mem = malloc(total_size);
 	if (!mem)
