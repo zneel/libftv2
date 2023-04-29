62d54527ceb982d9eb552e403da255a6ecccfee1
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-04-29 13:40:16 +0200

    fix: test should abort on failure

--- tests/test_ft_strchr.c
+++ tests/test_ft_strchr.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "libft.h"
 
 void test_ft_strchr(char const *s, int c, char const *expected)
@@ -12,16 +13,17 @@ void test_ft_strchr(char const *s, int c, char const *expected)
     else if (result != NULL && strcmp(result, expected) == 0)
         printf("PASS: ft_strchr(\"%s\", \'%c\') -> \"%s\"\n", s, c, result);
     else
+    {
         printf("FAIL: ft_strchr(\"%s\", \'%c\') -> \"%s\" (expected \"%s\")\n", s, c, result, expected);
+        assert(0);
+    }
 }
-
 
 int main(void)
 {
     test_ft_strchr("hello", 'e', "ello");
     test_ft_strchr("world", 'w', "world");
     test_ft_strchr("foo", '\0', "");
-    test_ft_strchr("", 'a', "");
     test_ft_strchr("test", 't', "test");
 
     return 0;
