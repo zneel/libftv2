bab797b4792e82f0b57787e32274acaaa5f8e4af
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-05-21 16:24:27 +0200

    add prototype

--- includes/libft.h
+++ includes/libft.h
@@ -6,62 +6,63 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 13:30:19 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/05/21 16:14:39 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/05/21 16:23:54 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <limits.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
-# include <limits.h>
 
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putendl_fd(char *s, int fd);
+void		ft_putnbr_fd(int n, int fd);
 
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
+size_t		ft_strlen(const char *s);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+char		*ft_strnstr(const char *big, const char *little, size_t len);
 
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strdup(const char *s);
 
-char	*ft_itoa(int n);
-int		ft_atoi(const char *s);
+char		*ft_itoa(int n);
+int			ft_atoi(const char *s);
+long long	ft_atoll(const char *s);
 
-char	**ft_split(char const *s, char c);
-char	*ft_strtrim(char const *s1, char const *set);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char		**ft_split(char const *s, char c);
+char		*ft_strtrim(char const *s1, char const *set);
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void		ft_striteri(char *s, void (*f)(unsigned int, char *));
 
-int		ft_isalnum(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_isprint(int c);
-int		ft_isdigit(int c);
-int		ft_isascii(int c);
-int		ft_isalpha(int c);
-int		ft_isspace(int c);
+int			ft_isalnum(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+int			ft_isprint(int c);
+int			ft_isdigit(int c);
+int			ft_isascii(int c);
+int			ft_isalpha(int c);
+int			ft_isspace(int c);
 
-void	*ft_calloc(size_t nmemb, size_t size);
+void		*ft_calloc(size_t nmemb, size_t size);
 
-void	*ft_memset(void *s, int c, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	ft_bzero(void *s, size_t n);
+void		*ft_memset(void *s, int c, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+void		ft_bzero(void *s, size_t n);
 
-int		is_sorted(int *tab, int length, int (*f)(int, int));
-void	quick_sort(int arr[], int low, int high);
+int			is_sorted(int *tab, int length, int (*f)(int, int));
+void		quick_sort(int arr[], int low, int high);
 
 #endif
