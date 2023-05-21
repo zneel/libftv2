81b3e7dd704299053743125495b362210fd5d0dc
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-05-21 14:25:27 +0200

    update is_sorted function

--- sources/algorithms/sorts/is_sorted.c
+++ sources/algorithms/sorts/is_sorted.c
@@ -6,7 +6,7 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 16:44:24 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/05/15 16:48:52 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/05/21 12:56:37 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,15 @@
 int	is_sorted(int *tab, int length, int (*f)(int, int))
 {
 	int	i;
-	int	way;
 
-	i = 1;
-	way = 0;
-	if (length == 1 || length == 0)
+	if (length <= 1)
 		return (1);
+	i = 1;
 	while (i < length)
 	{
 		if ((*f)(tab[i - 1], tab[i]) > 0)
-		{
-			if (way == -1)
-				return (0);
-			way = 1;
-		}	
-		if ((*f)(tab[i - 1], tab[i]) < 0)
-		{
-			if (way == 1)
-				return (0);
-			way = -1;
-		}
-		i++;
+			return (0);
+		++i;
 	}
 	return (1);
 }
